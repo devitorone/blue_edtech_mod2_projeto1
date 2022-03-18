3f4a5ce3f8ecb42ac3877fa6a530e60529d1cfea
devitorone <vitor@cazebre.com>
2022-03-18 15:36:15 -0300

    - background change js - allign voljin, button

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const btnTexto = document.querySelector("#btn");
-
+var count = 0;
 
 btnTexto.addEventListener("click", function () {
-//   // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
-//   const tagH1 = document.createElement("h3"); // Cria uma tag h2 e adiciona a let tagH1
-//   const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
+  count++;
+  //   // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
+  //   const tagH1 = document.createElement("h3"); // Cria uma tag h2 e adiciona a let tagH1
+  //   const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
 
-//   tagH1.appendChild(h1Text); // Adiciona à tag H1 o texto criado acima
+  //   tagH1.appendChild(h1Text); // Adiciona à tag H1 o texto criado acima
 
-//   document.body.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
-  document.body.style.backgroundImage =
-    "url('https://64.media.tumblr.com/ae5c280d009bc727a4fe171109bea128/tumblr_pgwyy9Sl8d1vikv9co1_1280.png')";
+  //   document.body.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
+  if (count == 1) {
+    document.body.style.backgroundImage = "url('./assets/img/DD-tavern.jpg')";
+  } else if (count == 2) {
+    document.body.style.backgroundImage =
+      "url('./assets/img/taverna_azeroth.jpg')";
+  } else if (count == 3) {
+    document.body.style.backgroundImage =
+      "url('./assets/img/taverna-the-geek-life.jpg')";
+  } else if (count == 4) {
+    alert("Vai com calma, nosso amigo Voljin não é de ferro. Amanhã é um outro dia");
+    count = 0;
+    document.body.style.backgroundImage =
+      "url('./assets/img/taverna1.jpg')";
+  }
 });
 
 // window.onload = function () {
-//   alert("Olá web, aqui é o javascript");
+//   document.body.style.backgroundImage ="url('./assets/img/Taverna.jpg')";
 // };
