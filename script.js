a346649490ab8640cab98949901c35010e8ca102
devitorone <vitor@cazebre.com>
2022-03-18 09:30:09 -0300

    background responsive add

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const btnTexto = document.querySelector("#btn-texto");
+const btnTexto = document.querySelector("#btn");
+
 
 btnTexto.addEventListener("click", function () {
-  // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
-  const tagH1 = document.createElement("h3"); // Cria uma tag h2 e adiciona a let tagH1
-  const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
+//   // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
+//   const tagH1 = document.createElement("h3"); // Cria uma tag h2 e adiciona a let tagH1
+//   const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
 
-  tagH1.appendChild(h1Text); // Adiciona à tag H1 o texto criado acima
+//   tagH1.appendChild(h1Text); // Adiciona à tag H1 o texto criado acima
 
-  document.body.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
-  document.body.style.background =
-    "url('http://www.blogcdn.com/wow.joystiq.com/media/2007/08/insiderzgenter.jpg') no-repeat center center";
+//   document.body.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
+  document.body.style.backgroundImage =
+    "url('https://64.media.tumblr.com/ae5c280d009bc727a4fe171109bea128/tumblr_pgwyy9Sl8d1vikv9co1_1280.png')";
 });
 
 // window.onload = function () {
